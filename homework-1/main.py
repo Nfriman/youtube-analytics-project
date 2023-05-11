e1bf8854c96a958ebc6c27ca1654681c02f95f53
admi <nariman.ramazaov@yandex.ru>
2023-05-11 12:02:06 +0300

    home work1 done

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,8 @@
 from src.channel import Channel
-
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
     vdud.print_info()
+    print(vdud.get_service())
 
     """
 {
--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,8 @@
+import json
 import os
-import requests
 from googleapiclient.discovery import build
 api_key = os.getenv('YT_API_KEY')
+
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -10,8 +11,40 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        self.title = self.info['items'][0]['snippet']['title']
+        self.description = self.info['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA'
+        self.subscriber_count = self.info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.info['items'][0]['statistics']['videoCount']
+        self.view_count = self.info['items'][0]['statistics']['viewCount']
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey="AIzaSyAAFIjHIkwcu7HPEs_7sCje4Aa4LvyPAOQ")
+        youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(channel)
+
+    def to_json(self, json_file):
+        data = {
+            "id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriber_count": self.subscriber_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+
+        }
+
+        with open(json_file, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        return data
+
+    @classmethod
+    def get_service(cls):
+        """возвращающий объект для работы с YouTube API"""
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
